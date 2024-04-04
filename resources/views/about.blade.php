fe154942c8e7019218b647f7ec730b6af69e9198
zinhour10 <zinhour10@gmail.com>
2024-04-04 23:00:22 +0700

    about pahe

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,9 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="/css/about.css">
+    <title>About</title>
 </head>
 <body>
-    <h1>hello</h1>
+    <nav class="navbar">
+        <p>NextGen</p>
+    </nav>
+    <div class="content">
+        <p>OUR TEAM</p>
+        <div class="teamOpt">
+            <div class="opt">
+                <div class="member hour"></div>
+                <p>CHHIN MENGHOUR</p>
+            </div>
+            <div class="opt">
+                <div class="member panhar"></div>
+                <p>NANGDY PANHAR</p>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
