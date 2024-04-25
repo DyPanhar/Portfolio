6f9fc4e60bb2eba1a0d7209b61adc845b57a6270
zinhour10 <zinhour10@gmail.com>
2024-04-25 12:56:16 +0700

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,24 +4,57 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/about.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>About</title>
 </head>
 <body>
+    <div id="navMenu" class="overlayClose">
+        <a href="">ABOUT</a>
+        <a href="">RESUME</a>
+        <a href="">WORK</a>
+    </div>
     <nav class="navbar">
         <p>NextGen</p>
+        <span onclick="openNav()">
+        <div class="menu" id="menu">
+            <div class="one"></div>
+            <div class="two"></div>
+        </div>
+        </span>
     </nav>
-    <div class="content">
-        <p>OUR TEAM</p>
-        <div class="teamOpt">
-            <div class="opt">
-                <div class="member hour"></div>
-                <p>CHHIN MENGHOUR</p>
-            </div>
-            <div class="opt">
-                <div class="member panhar"></div>
-                <p>NANGDY PANHAR</p>
+    <div class="container-fluid">
+        <div class="content">
+            <p>OUR TEAM</p>
+            <div class="teamOpt">
+                <div class="opt">
+                    <div class="member hour">
+                        <img src="image/hour.png" alt="">
+                    </div>
+                    <p>CHHIN MENGHOUR</p>
+                </div>
+                <div class="opt">
+                    <div class="member panhar">
+                        <img src="image/hour.png" alt="">
+                    </div>
+                    <p>NANGDY PANHAR</p>
+                </div>
             </div>
         </div>
     </div>
 </body>
 </html>
+<script>
+function openNav() {
+    if(document.getElementById("menu")){
+        document.getElementById("navMenu").className="overlayOpen";
+        document.getElementById("menu").className="menu close"; 
+        document.getElementById("menu").setAttribute("id", "closeMenu"); 
+    }
+    else if(document.getElementById("closeMenu")){
+        document.getElementById("navMenu").className="overlayClose";
+        document.getElementById("closeMenu").className="menu"; 
+        document.getElementById("closeMenu").setAttribute("id", "menu"); 
+    }
+}
+
+</script>
---
+++ resources/views/menghour.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/menghour.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>About</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <h1>NextGen</h1>
+        </div>
+        <div class="navMenu">
+            <ul>
+                <li>About</li>
+                <li>Resume</li>
+                <li>Work</li>
+            </ul>
+        </div>
+    </nav>
+    <div class="profile">
+        <div class="left-contain">
+            <h1>Hello,<br>I’m HOUR</h1>
+            <p>My Name Chhin Menghour. I am a student<br> In Cadt. I am a hard-working and driven<br> individual who isn't afraid to face a<br> challenge.</p>
+        </div>
+        <div class="right-contain">
+            <img src="image/hour.png" alt="" class="img-responsive">
+        </div>
+    </div>
+    <div class="backgroundProfile"></div>
+    <div class="experience">
+        <div class="education">
+            <h1>Education</h1>
+            <p><b>2022-present : </b>Cambodia Academy Degital Technology<br> Computer Science</p>
+        </div>
+        <div class="exp">
+            <h1>Experience</h1>
+            <p><b>2022-present : </b>ABA Bank<br>Data Analysis</p>
+            <div class="softskill">
+                <div class="skill">
+                    #creative
+                </div>
+                <div class="skill">
+                    #Communication
+                </div>
+                <div class="skill">
+                    #Critical Thinking
+                </div>
+            </div>
+        </div>
+        <div class="resumebackground">
+            <h1 class="one">RESUME</h1>
+            <h1 class="two">RESUME</h1>
+            <h1 class="three">RESUME</h1>
+            <h1 class="four">RESUME</h1>
+        </div>
+        <div class="language">
+            <h1>Language</h1>
+            <div class="languages">
+                <p>Khmer</p>
+                <p>English</p>
+            </div>
+        </div>
+        <div class="backgroundName">
+            <span>
+            <h1>CHHIN&nbsp;MENGHOUR</h1>
+            <h1>CHHIN&nbsp;MENGHOUR</h1>
+            <h1>CHHIN&nbsp;MENGHOUR</h1>
+            </span>
+        </div>
+    </div>
+</body>
+</html>
